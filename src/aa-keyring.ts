6470f7e2864f5dc2ca02d3fdb7add0aa46614134
ChiHaoLu <b08303113@ntu.edu.tw>
2024-01-30 12:02:27 +0800

    add serializer and de

--- src/aa-keyring.ts
+++ src/aa-keyring.ts
@@ -10,19 +10,51 @@ import {
 import { add0x, Eip1024EncryptedData, Hex, Keyring } from "@metamask/utils";
 import SimpleKeyring from "./simple-keyring";
 
-export default class SimpleAAKeyring extends SimpleKeyring {
-  #aawallets: { privateKey: Buffer; publicKey: Buffer; contractAddr: string }[];
+type AAWallet = {
+    privateKey: Buffer;
+    publicKey: Buffer;
+    contractAddr: Hex 
+}
 
-  constructor(privateKeys: string[] = [], type: string = "Simple AA Keyring") {
+export default class SimpleAAKeyring extends SimpleKeyring {
+  #aawallets: AAWallet[];
+
+  constructor(
+    privateKeys: string[] = [],
+    contractAddrs: Hex[] = [],
+    type: string = "Simple AA Keyring"
+  ) {
     super(privateKeys, type);
     this.#aawallets = [];
+    /* istanbul ignore next: It's not possible to write a unit test for this, because a constructor isn't allowed
+     * to be async. Jest can't await the constructor, and when the error gets thrown, Jest can't catch it. */
+    this.deserializeAAWallets(privateKeys).catch(
+      (error: Error) => {
+        throw new Error(`Problem deserializing SimpleKeyring ${error.message}`);
+      }
+    );
   }
 
-  async addAAAccounts(
-    numAccounts = 1,
-    privateKey: Buffer,
-    contractAddr: string,
-  ) {
+  async serializeAAWallets() {
+    return this.#aawallets.map((a) => a.privateKey.toString("hex"));
+  }
+
+  async deserializeAAWallets(privateKeys: string[] = []) {
+    this.#aawallets = privateKeys
+      .map((hexPrivateKey) => {
+        const strippedHexPrivateKey = stripHexPrefix(hexPrivateKey);
+        const privateKey = Buffer.from(strippedHexPrivateKey, "hex");
+        const publicKey = privateToPublic(privateKey);
+        const contractAddr = this.#aawallets.find((wallet) =>
+          wallet.privateKey.equals(privateKey)
+        )?.contractAddr;
+
+        return { privateKey, publicKey, contractAddr };
+      })
+      .filter((wallet): wallet is AAWallet => !!wallet);
+  }
+
+  async addAAAccounts(numAccounts = 1, privateKey: Buffer, contractAddr: Hex) {
     const newWallets = [];
     for (let i = 0; i < numAccounts; i++) {
       const publicKey = privateToPublic(privateKey);
@@ -30,7 +62,7 @@ export default class SimpleAAKeyring extends SimpleKeyring {
     }
     this.#aawallets = this.#aawallets.concat(newWallets);
     const hexWallets = newWallets.map(({ publicKey }) =>
-      add0x(bufferToHex(publicToAddress(publicKey))),
+      add0x(bufferToHex(publicToAddress(publicKey)))
     );
     return hexWallets;
   }
